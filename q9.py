884e35f82346a7b5849a7970c927e889773598dc
psheru <111558793+psheru@users.noreply.github.com>
2022-08-29 10:51:03 +0530

    Add files via upload

---
+++ q9.py
@@ -0,0 +1,6 @@
+l=int(input("enter the length of cuboid:-"))
+b=int(input("enter the breadth of cuboid:-"))
+h=int(input("enter the height of cuboid:-"))
+vol=l*b*h
+print('volume of the cuboid is:-',vol)
+
